d62f1ea69f4b1184a871f20be2f1a11018d31e21
Rebecca Appleyard <rklappleyard@gmail.com>
2015-09-18 10:36:35 +0100

    Beginning to add username functionality

--- public/app.js
+++ public/app.js
@@ -2,8 +2,17 @@
 //by not specifying a url to connect to, it defaults to the host that serves the page
 $(document).ready(function() {
   var socket = io();
+  var username;
 
-  $('form').submit(function() {
+  $('.start').click(function(event) {
+    event.preventDefault();
+    $('.welcome').hide();
+    $('.chatbox').show();
+    username = $('.name').val();
+    console.log(username);
+  });
+
+  $('.chat').submit(function() {
     //send the chat message - note it hasn't been written as json - it formats that itself?
     socket.emit('chat message', $('#m').val());
     $('#m').val('');
